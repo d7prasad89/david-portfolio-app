27f8ab4de73801b286e6dbe32eb88020cb277cff
David Prasad <d7prasad89@gmail.com>
2025-05-17 16:26:03 -0400

    Portfolio Hero Section Development in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,35 @@
 import './App.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import ProfilePicture from "../src/assets/Profile_Picture.png"
 
 function App() {
 
   return (
       <>
-          <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-              <div className="text-center">
-                  <p className="text-base font-semibold text-indigo-600">David Prasad Augustian</p>
-                  <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-                      Passionate about building Web/Mobile based Applications<br />
-                  </h1>
-                  <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-                      Trying to build a better web for everyone. <br />
-                  </p>
-                  <div className="mt-10 flex items-center justify-center gap-x-6">
-                      <a
-                          href="#"
-                          className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                      >
-                          Go back home
-                      </a>
-                      <a href="#" className="text-sm font-semibold text-gray-900">
-                          Contact support <span aria-hidden="true">&rarr;</span>
-                      </a>
+          <div className="hero min-h-screen main-hero">
+              <div className="hero-content flex-col lg:flex-row">
+                  <div className="avatar">
+                      <div className="ring-primary ring-offset-base-100 w-60 rounded-full ring-2 ring-offset-2">
+                          <img src={ProfilePicture}/>
+                      </div>
                   </div>
+                  <div className={"m-auto"}>
+                      <h1 className="text-5xl font-bold">David Prasad Augustian</h1>
+
+                      <i>Blessed to be a Blessing - Genesis 12:2</i> <br/><br/>
+                      <p className="py-6">
+
+                          I am a software engineer with a passion for building web applications. I have experience in
+                          Java, Spring-boot, React, and Node.js. I am currently learning Golang and Kubernetes. I am
+                          also interested in cloud computing and DevOps practices.
+
+                      </p>
+                      <button className="btn btn-primary">Get Started</button>
+                  </div>
+
               </div>
-          </main>
+          </div>
       </>
   )
 }
